58f4297e950ec62cf976750d9cb0d41395ece90a
Ahsan Ulhaq <ahsan.haq@arbisoft.com>
2018-09-24 17:27:26 +0500

    Added SessionAuthenticationAllowInactiveUser LEARNER-6423

---
+++ edx_rest_framework_extensions/tests/test_authentication.py
@@ -0,0 +1,27 @@
+""" Test for SessionAuthenticationAllowInactiveUser class """
+from django.test import RequestFactory, TestCase
+
+from edx_rest_framework_extensions.tests import factories
+from edx_rest_framework_extensions.authentication import SessionAuthenticationAllowInactiveUser
+
+
+class SessionAuthenticationAllowInactiveUserTests(TestCase):
+    def setUp(self):
+        super(SessionAuthenticationAllowInactiveUserTests, self).setUp()
+        self.user = factories.UserFactory(
+            email='inactive', username='inactive@example.com', password='dummypassword', is_active=False
+        )
+        self.request = RequestFactory().get('/')
+
+    def test_authenticate(self):
+        """Verify inactive user is authenticated."""
+        self.request.user = self.user
+        self.request._request = self.request
+        user, _ = SessionAuthenticationAllowInactiveUser().authenticate(self.request)
+        self.assertEqual(user, self.user)
+
+    def test_user_not_exist(self):
+        """Verify request with no user return None."""
+        self.request._request = self.request
+        user = SessionAuthenticationAllowInactiveUser().authenticate(self.request)
+        self.assertEqual(user, None)
