00185b9ee41ef3cee78d8985d3c351fad6ad7aa1
Nimisha Asthagiri <nasthagiri@edx.org>
2019-03-11 20:26:22 -0400

    Remove jwt_refresh_cookie
    The login_refresh endpoint in the LMS was updated to be based on the
    user's session, rather than requiring an additional refresh cookie, so
    removing any references to the unused refresh cookie and
    JWT_AUTH_REFRESH_COOKIE setting.
    
    ARCH-418

--- edx_rest_framework_extensions/auth/jwt/cookies.py
+++ edx_rest_framework_extensions/auth/jwt/cookies.py
@@ -19,10 +19,6 @@ def jwt_cookie_signature_name():
     return settings.JWT_AUTH.get('JWT_AUTH_COOKIE_SIGNATURE') or 'edx-jwt-cookie-signature'
 
 
-def jwt_refresh_cookie_name():
-    return settings.JWT_AUTH.get('JWT_AUTH_REFRESH_COOKIE') or 'edx-jwt-refresh-cookie'
-
-
 def get_decoded_jwt(request):
     """
     Grab jwt from jwt cookie in request if possible.
