b05eaa70f290bdcce03714e7d0595825ba18f8ab
Robert Raposa <rraposa@edx.org>
2021-02-11 23:53:28 -0500

    fix: update toggle annotations

--- edx_rest_framework_extensions/config.py
+++ edx_rest_framework_extensions/config.py
@@ -6,11 +6,9 @@ Application configuration constants and code.
 # .. toggle_implementation: DjangoSetting
 # .. toggle_default: False
 # .. toggle_description: Toggle for setting request.user with jwt cookie authentication
-# .. toggle_category: micro-frontend
-# .. toggle_use_cases: incremental_release  # pylint: disable=annotation-invalid-choice
+# .. toggle_use_cases: temporary
 # .. toggle_creation_date: 2019-10-15
-# .. toggle_expiration_date: 2019-12-31
+# .. toggle_target_removal_date: 2019-12-31
 # .. toggle_warnings: This feature fixed ecommerce, but broke edx-platform. The toggle enables us to fix over time.
 # .. toggle_tickets: ARCH-1210, ARCH-1199, ARCH-1197
-# .. toggle_status: supported
 ENABLE_SET_REQUEST_USER_FOR_JWT_COOKIE = 'ENABLE_SET_REQUEST_USER_FOR_JWT_COOKIE'
